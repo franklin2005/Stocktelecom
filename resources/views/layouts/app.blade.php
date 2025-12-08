84f26b8b1c2597ebabd3430898d1d429426cc190
Franklin <franklineleazar20gmail.com>
2025-12-08 11:02:35 +0100

    Quitar importación de vite en el app.blade y separar estilos a mainStyle.css para corregir error en vistas de Admin/transfer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,11 @@
     {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
-    {{-- Estilos del proyecto --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- estilos personalizados--}}
+    <link rel="stylesheet" href="{{ asset('css/mainStyle.css') }}">
+
+
+
 
 </head>
 <body class="bg-light">
